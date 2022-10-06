95eb8c979ea6b8d9cfd2b09b0f51f29de4c28b9b
Ezema Anthony Sunday <tonyunday@gmail.com>
2022-10-06 09:02:13 -0700

    added new list_function.js to perform some add and rmove task

--- src/list_functions.test.js
+++ src/list_functions.test.js
@@ -1,27 +1,27 @@
 const { 
   add, edit, erase, clear 
-} = require("./list_functions.js");
+} = require('./list_functions.js');
 
 const text = 'some test';
 
 describe('add functions method', () => {
   beforeEach(() => {});
-  it("should be able to add a new task to the list ", () => {
+  it('should be able to add a new task to the list ', () => {
     const arr = [];
     add(text, arr);
     expect(arr.length).toBe(1);
   });
 });
-describe("edit functions method", () => {
-  it("should be able to chnage the description of the array to text passed in", () => {
+describe('edit functions method', () => {
+  it('should be able to chnage the description of the array to text passed in', () => {
     const arr = [
-      { description: "first text", completed: false, index: 0 },
+      { description: 'first text', completed: false, index: 0 },
       {
-        description: "second text",
+        description: 'second text',
         completed: false,
         index: 1,
       },
-      { description: "third text", completed: false, index: 2 },
+      { description: 'third text', completed: false, index: 2 },
     ];
     edit(text, arr, 0);
     edit(text, arr, 1);
@@ -32,16 +32,16 @@ describe("edit functions method", () => {
   });
 });
 
-describe("erase functions method", () => {
-  it("this should be able to remove a specific task from the array", () => {
+describe('erase functions method', () => {
+  it('this should be able to remove a specific task from the array', () => {
     const arr = [
-      { description: "first text", completed: false, index: 0 },
+      { description: 'first text', completed: false, index: 0 },
       {
-        description: "second text",
+        description: 'second text',
         completed: false,
         index: 1,
       },
-      { description: "third text", completed: false, index: 2 },
+      { description: 'third text', completed: false, index: 2 },
     ];
     erase(arr, 1);
     erase(arr, 2);
@@ -49,13 +49,13 @@ describe("erase functions method", () => {
     expect(arr.length).toBe(0);
   });
 });
-describe("clear functions method", () => {
-  it("this should be able to clear all items in the array that completed is true", () => {
+describe('clear functions method', () => {
+  it('this should be able to clear all items in the array that completed is true', () => {
     const arr = [
-      { description: "first text", completed: true, index: 0 },
-      { description: "second text", completed: true, index: 1 },
+      { description: 'first text', completed: true, index: 0 },
+      { description: 'second text', completed: true, index: 1 },
       {
-        description: "third text",
+        description: 'third text',
         completed: false,
         index: 2,
       },
